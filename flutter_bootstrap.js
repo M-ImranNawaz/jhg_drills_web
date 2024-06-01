ea9d0c1eb5e6d18426b2c739a8d7e6e651253fff
Mohammad Imran Nawaz <imrannawaz288@gmail.com>
2024-06-01 16:19:11 +0500

    test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1842124246"
+    serviceWorkerVersion: "3244283810"
   }
 });
